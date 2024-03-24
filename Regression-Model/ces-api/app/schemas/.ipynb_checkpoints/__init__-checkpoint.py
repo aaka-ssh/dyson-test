efe75fcc60c692d6fdf1afdd66bd239424ac436e
aaka-ssh <jupyter@my-dy-test.us-central1-a.c.useful-aquifer-416009.internal>
2024-03-24 15:46:21 +0000

    testing cicd3

--- Regression-Model/ces-api/app/schemas/.ipynb_checkpoints/__init__-checkpoint.py
+++ Regression-Model/ces-api/app/schemas/.ipynb_checkpoints/__init__-checkpoint.py
@@ -1,2 +1,2 @@
-from .health import Health # noqa: F401
-from .predict import MultipleCesDataInputs, PredictionResults # noqa: F401
+from .health import Health  # noqa: F401
+from .predict import MultipleCesDataInputs, PredictionResults  # noqa: F401
