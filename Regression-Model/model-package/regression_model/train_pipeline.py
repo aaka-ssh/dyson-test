032a6156063f715bab4e0e99c0f08a1d13381051
aaka-ssh <111515843+aaka-ssh@users.noreply.github.com>
2024-03-30 15:29:54 +0530

    Update train_pipeline.py

--- Regression-Model/model-package/regression_model/train_pipeline.py
+++ Regression-Model/model-package/regression_model/train_pipeline.py
@@ -8,10 +8,8 @@ from sklearn.model_selection import train_test_split
 def run_training() -> None:
     try:
         """Train the model."""
-    
         # read training data
         data = load_dataset(file_name=config.app_config.training_data_file)
-    
         # divide train and test
         X_train, X_test, y_train, y_test = train_test_split(
             data[config.model_config.features],  # predictors
@@ -22,10 +20,8 @@ def run_training() -> None:
             random_state=config.model_config.random_state,
         )
         y_train = np.log(y_train)
-    
         # fit model
         ces_pipe.fit(X_train, y_train)
-    
         # persist trained model
         save_pipeline(pipeline_to_persist=ces_pipe)
     except Exception as e:
