7aabdf58e31588cd1e3c6dff48f5d6637a508c25
aaka-ssh <111515843+aaka-ssh@users.noreply.github.com>
2024-03-30 15:15:42 +0530

    Update train_pipeline.py

--- Regression-Model/model-package/regression_model/train_pipeline.py
+++ Regression-Model/model-package/regression_model/train_pipeline.py
@@ -7,29 +7,29 @@ from sklearn.model_selection import train_test_split
 
 def run_training() -> None:
     try:
-    """Train the model."""
-
-    # read training data
-    data = load_dataset(file_name=config.app_config.training_data_file)
-
-    # divide train and test
-    X_train, X_test, y_train, y_test = train_test_split(
-        data[config.model_config.features],  # predictors
-        data[config.model_config.target],
-        test_size=config.model_config.test_size,
-        # setting the random seed here
-        # for reproducibility
-        random_state=config.model_config.random_state,
-    )
-    y_train = np.log(y_train)
-
-    # fit model
-    ces_pipe.fit(X_train, y_train)
-
-    # persist trained model
-    save_pipeline(pipeline_to_persist=ces_pipe)
+        """Train the model."""
+    
+        # read training data
+        data = load_dataset(file_name=config.app_config.training_data_file)
+    
+        # divide train and test
+        X_train, X_test, y_train, y_test = train_test_split(
+            data[config.model_config.features],  # predictors
+            data[config.model_config.target],
+            test_size=config.model_config.test_size,
+            # setting the random seed here
+            # for reproducibility
+            random_state=config.model_config.random_state,
+        )
+        y_train = np.log(y_train)
+    
+        # fit model
+        ces_pipe.fit(X_train, y_train)
+    
+        # persist trained model
+        save_pipeline(pipeline_to_persist=ces_pipe)
     except Exception as e:
-    print("An error occurred during training:", str(e))
+        print("An error occurred during training:", str(e))
 
 
 if __name__ == "__main__":
